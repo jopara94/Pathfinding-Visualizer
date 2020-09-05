83718e142a0844f1c8bd2c071817831332474646
James Opara <jopara94@gmail.com>
2020-09-05 11:39:55 -0500

    structure

---
+++ pathfinding-visualizer/src/Node.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Node.css";
+
+class Node extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="Node">Yessir</div>;
+  }
+}
+
+export default Node;
